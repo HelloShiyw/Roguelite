5a83724224672d9359daa27635546936383abca6
SaarKeynan <44527391+SaarKeynan@users.noreply.github.com>
2020-07-29 01:39:39 +0300

    Gun shooty point fixed
    moved it to end of gun. still have to figure out how to do that better for the other guns

--- Roguelite/Assets/Scripts/PlayerShoot.cs
+++ Roguelite/Assets/Scripts/PlayerShoot.cs
@@ -6,6 +6,7 @@ public class PlayerShoot : MonoBehaviour
 {
     public GameObject gunObject;
     public GameObject bullet;
+    public Transform bulletPoint;
     // Update is called once per frame
     void Update()
     {
@@ -14,8 +15,7 @@ public class PlayerShoot : MonoBehaviour
             GameObject bulletClone = Instantiate(bullet);
             bulletClone.GetComponent<BulletPhysics>().enabled = true;
             bulletClone.GetComponent<Rigidbody2D>().simulated = true;
-            bulletClone.transform.position = transform.position;
-            bulletClone.transform.position = gunObject.transform.position;
+            bulletClone.transform.position = bulletPoint.position;
             bulletClone.transform.rotation = gunObject.transform.rotation;
             Rigidbody2D rb = bulletClone.GetComponent<Rigidbody2D>();
             rb.AddForce(bulletClone.transform.right*gunObject.GetComponent<GunDisplay>().gun.bulletForce, ForceMode2D.Impulse);
