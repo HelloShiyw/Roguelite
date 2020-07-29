7fd6ecb494bb547fe4c27700271c8dec4777000a
SaarKeynan <44527391+SaarKeynan@users.noreply.github.com>
2020-07-29 12:05:17 +0300

    p much nothing

--- Roguelite/Assets/Bullet.cs
+++ Roguelite/Assets/Bullet.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Bullet", menuName = "Bullet")]
-
 public class Bullet : ScriptableObject
 {
     public string sprite, shotType; //shot types may be spray and normal?
